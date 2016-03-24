a27be92fe4489446de796c9c28f186e138f5ba64
victorialice <1506675573@qq.com>
2016-03-24 23:53:47 +0800

    modify ajax function

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,10 @@
+$(function() {
+  $("body").on("click", "#products th a, #products .pagination a", function() {
+    $.getScript(this.href);
+    return false;
+  });
+  $("#products_search input").keyup(function() {
+    $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+    return false;
+  });
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+strings = ["alice", "Jack", "Smith"]
+1..100.times do |i|
+  Product.create(
+    name: strings[rand(3)] + i.to_s,
+    price: rand(550),
+    released_at: DateTime.now - rand(10).day
+  )
+end
